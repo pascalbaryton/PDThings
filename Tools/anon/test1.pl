3ea083389ab95ac02d8d72043827b48db77fca3d
pascal <pmenoud@gmail.com>
2015-10-27 13:32:12 +0100

    Add initial files

---
+++ Tools/anon/test1.pl
@@ -0,0 +1,27 @@
+#
+   use strict;
+   use Anonymizer;
+   use Test::Simple tests=>12; # 2 + 5 * #models
+
+   my @models = (
+      'test2.oom',
+      'test1.eam'
+   );
+
+   ok(&Anonymizer::CheckXMLModel('Anonymizer.pm')==0, 'detect a bad file');
+   foreach my $m (@models) {
+      ok(&Anonymizer::CheckXMLModel($m)==1, "recognize PD model $m");
+   }
+
+   ok(&Anonymizer::LoadXMLModel('xxx') eq '', 'process missing model error');
+   foreach my $m (@models) {
+      my $read = &Anonymizer::LoadXMLModel($m);
+      ok($read ne '' && $read =~ /PowerDesigner/, "load PD model $m");
+   
+      my $changed = &Anonymizer::ChangeXMLModel($read);
+      ok($changed ne '', 'changing model contents');
+      ok(length($read) == length($changed), 'preserve model length');
+      ok($changed ne $read, 'contents changed');
+   }
+
+   &Anonymizer::DumpNewAttributes;
